240d40bda1464c0def334ee5abcdbdc8aa4b8f23
tisuama <1754121135@qq.com>
2022-04-11 03:05:55 +0800

    [Upate] Chartp2

--- Chartp2/code.cpp
+++ Chartp2/code.cpp
@@ -10,4 +10,12 @@ int main() {
 	const int& r1 = ci;
 	// int& r2 = ci; // 错误，非常量引用绑定一个常量对象	
 	const int& xi = 1024; // 这里绑定右值是可以的
+
+	// 引用
+	// int& ref = 10;// 引用必须是对象
+	int a = 1;
+	int& ref = a;
+	
+	const int& ref_1 = a;
+	const int& ref_2 = 10;
 }
