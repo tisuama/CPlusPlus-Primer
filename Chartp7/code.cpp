9c67d8b9d13326c8c5498bd7c4e1c5713c7d7061
tisuama <1754121135@qq.com>
2022-05-21 01:41:53 +0800

    [Upate] Chartp7

--- Chartp7/code.cpp
+++ Chartp7/code.cpp
@@ -1,35 +1,47 @@
 #include <iostream>
 
-struct Sales_data {
+class Sales_data {
 	std::string isbn() const { return bookNo; }
 	Sales_data& combine(const Sales_data&);
 	double avg_price() const;
-	
+
 	std::string bookNo;
-	unsigned units_slod = 0;
+	unsigned units_sold = 0;
 	double revenue = 0;
 };
 
-class A {
-public:
-	explicit A(int x): a(x) {}
-private:
-	int x;
-};
-
-class B {
-private:
-	A a;
-};
 
 Sales_data& Sales_data::combine(const Sales_data& rhs) {
-	units_slod += rhs.units_slod;
+	units_sold += rhs.units_sold;
 	revenue += rhs.revenue;
 	return *this;
+}
+
+class Sales_data1 {
+	friend Sales_data1 add(const Sales_data1&, const Sales_data1&);
+	friend std::istream& read(std::istream&, Sales_data1&);
+public:
+	Sales_data1() = default;
+	Sales_data1(const std::string& s, unsigned n, double p): 
+		bookNo(s), units_sold(n), revenue(n * n) {}
+	Sales_data1(const std::string& s): bookNo(s) {}
+	Sales_data1(std::istream&);
+	std::string isbn() const { return bookNo; }
+	Sales_data1& combine(const Sales_data1&);
+
+private:
+	std::string  bookNo;
+	unsigned units_sold = 0;	
+	double revenue = 0.0;
+};
+
+int Add(int x, int y) {
+	return x + y;
 }
 
 int main() {
 	Sales_data total;
 	// total.combine(trans); // 等价于返回total的引用
-	B b;
+	int x, y;
+	constexpr int z = Add(x, y);
 }
