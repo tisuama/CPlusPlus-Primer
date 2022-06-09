9f9c4f4ace4f09515a7d83d1bedd7e5e1bc3e637
tisuama <1754121135@qq.com>
2022-06-10 00:57:44 +0800

    [Update] Chartp13

--- Chartp13/code.cpp
+++ Chartp13/code.cpp
@@ -10,7 +10,31 @@ struct D { // 删除器
 	D& operator=(D&& d) { std::cout << "D move assign" << std::endl; return *this; }
 };
 
+class Sales_data {
+public:
+	Sales_data() = default;
+	void set_value(int p, int x) {
+		assert(p < 20);
+		A[p] = x;
+	}
+	int get_value(int p) {
+		assert(p < 20);
+		return A[p];
+	}
+private:
+	std::string bookNo;
+	int units_sold = 0;
+	double revenue = 0.0;
+	int A[20];
+};
+
+
 int main() {
 	D d;
 	d = D(1);
+	
+	Sales_data s;
+	s.set_value(1, 20);
+	Sales_data cs(s);
+	std::cout << cs.get_value(1) << std::endl;
 }
