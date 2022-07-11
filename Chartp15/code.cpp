c737d51ee276a395e63b3de28cdf99fd7ef8239a
tisuama <1754121135@qq.com>
2022-07-12 00:15:41 +0800

    [Update] update Chartp15

--- Chartp15/code.cpp
+++ Chartp15/code.cpp
@@ -31,5 +31,21 @@ private:
 	double discount = 0.0;
 };
 
+class Disc_quote: public: Quote {
+public:
+	Disc_quote() = default;
+	Disc_quote(const std::string& book, double price, 
+			std::size_t qty, double disc): 
+		Quote(book, price),
+		quantity(qty),
+		discount(disc)
+	{}
+
+	double net_price(std::size_t) const = 0;
+private:
+	std::size_t quantity = 0;
+	double discount = 0.0;
+};
+
 int main() {
 }
