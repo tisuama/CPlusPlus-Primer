5a39bc099caa3a4d936bf81eb270e669e5f078ec
tisuama <1754121135@qq.com>
2022-07-28 00:13:41 +0800

    [Update] update Chartp16

--- Chartp16/code.cpp
+++ Chartp16/code.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <functional>
 
+/*
 template<typename T>
 int compare(const T &v1, const T& v2) {
 	if (v1 < v2) return -1;
 	if (v2 < v1) return 1;
 	return 0;
 }
+*/
+
+template<unsigned N, unsigned M>
+int compare(const char(&p1)[N], const char (&p2)[M]) {
+	return strcmp(p1, p2);
+}
+
+template<typename T>
+int compare1(const T& v1, const T& v2) {
+	if (std::less<T>{}(v1, v2)) return -1;
+	if (std::less<T>{}(v2, v1)) return 1;
+	return 0;
+}
 
 int main() {
-	std::cout << compare(1, 2) << std::endl;
+	// std::cout << compare(1, 2) << std::endl;
+	std::cout << compare("haha", "hehe") << std::endl;
+	std::cout << compare1(1, 2) << std::endl;
 }
 
